8ae3be96ab496513efb91aa5b2c3513daf25ebb3
Matt Goldfarb <goldfarb@google.com>
2015-11-18 10:19:48 -0500

    updating

---
+++ installlinks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="vimrc tmux.conf tmux.reset.conf"    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done

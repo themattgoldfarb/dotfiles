c18c8d13aa81765e17f5c55665ba37ddb49cdd42
Matt Goldfarb <goldfarb@google.com>
2020-03-16 18:53:54 +0000

    add symlink for nvim

--- installstuff.sh
+++ installstuff.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo "Checking vim plug."
 if [[ -e "$HOME/.vim/autoload/plug.vim" ]] ; then
     echo "Vim plug already exists."
@@ -8,6 +7,14 @@ else
     echo "Installing vim plug"
     curl -flo ~/.vim/autoload/plug.vim --create-dirs \
           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+echo "Checking nvim symlink"
+if [[ -e "$HOME/.config/nvim" ]] ; then
+    echo "nvim already linked"
+else
+    echo "Setting up symlink"
+    ln -s "$HOME/.vim" "$HOME/.config/nvim"
 fi
 
 packages=("xdotool")
